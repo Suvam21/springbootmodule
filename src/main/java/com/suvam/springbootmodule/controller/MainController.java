c9c43e39e51c174070b98cbc150f606ab0b94e31
Suvam21 <suvambehera21@gmail.com>
2023-07-26 17:25:37 +0530

    second commit

--- src/main/java/com/suvam/springbootmodule/controller/MainController.java
+++ src/main/java/com/suvam/springbootmodule/controller/MainController.java
@@ -1,11 +1,8 @@
 package com.suvam.springbootmodule.controller;
 
-// import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-// import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -14,7 +11,7 @@ public class MainController {
     @GetMapping("/")
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index.html");
+        modelAndView.setViewName("AccountsHandler.html");
         return modelAndView;
     }
     
